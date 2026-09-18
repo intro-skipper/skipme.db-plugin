a22657a7af747c0fda6d45248166ec045f587381
TwistedUmbrellaX <twistedumbrella@gmail.com>
2026-09-17 23:21:23 -0400

    Suppress collection property analyzer warnings

--- SkipMe.Db.Plugin/Configuration/PluginConfiguration.cs
+++ SkipMe.Db.Plugin/Configuration/PluginConfiguration.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 using MediaBrowser.Model.Plugins;
 
 namespace SkipMe.Db.Plugin.Configuration;
@@ -16,6 +17,10 @@ public class PluginConfiguration : BasePluginConfiguration
     /// Gets or sets the set of Jellyfin series item IDs for which crowd-sourced segments are disabled.
     /// When a series ID is present, no segments will be surfaced for any episode in that series.
     /// </summary>
+    [SuppressMessage(
+        "Microsoft.Usage",
+        "CA2227:CollectionPropertiesShouldBeReadOnly",
+        Justification = "Jellyfin must be able to deserialize the saved plugin configuration.")]
     public Collection<Guid> DisabledSeriesIds { get; set; } = [];
 
     /// <summary>
@@ -23,17 +28,29 @@ public class PluginConfiguration : BasePluginConfiguration
     /// When a season ID is present, no segments will be surfaced for any episode in that season,
     /// unless the containing series is already disabled.
     /// </summary>
+    [SuppressMessage(
+        "Microsoft.Usage",
+        "CA2227:CollectionPropertiesShouldBeReadOnly",
+        Justification = "Jellyfin must be able to deserialize the saved plugin configuration.")]
     public Collection<Guid> DisabledSeasonIds { get; set; } = [];
 
     /// <summary>
     /// Gets or sets the set of Jellyfin movie item IDs for which crowd-sourced segments are disabled.
     /// When a movie ID is present, no segments will be surfaced for that movie.
     /// </summary>
+    [SuppressMessage(
+        "Microsoft.Usage",
+        "CA2227:CollectionPropertiesShouldBeReadOnly",
+        Justification = "Jellyfin must be able to deserialize the saved plugin configuration.")]
     public Collection<Guid> DisabledMovieIds { get; set; } = [];
 
     /// <summary>
     /// Gets or sets the set of Jellyfin season item IDs for season 0 (Specials) that have been explicitly enabled.
     /// Specials seasons are disabled by default; a season ID must be present here for its segments to be surfaced.
     /// </summary>
+    [SuppressMessage(
+        "Microsoft.Usage",
+        "CA2227:CollectionPropertiesShouldBeReadOnly",
+        Justification = "Jellyfin must be able to deserialize the saved plugin configuration.")]
     public Collection<Guid> EnabledSpecialsSeasonIds { get; set; } = [];
 }
